0ea08d4b7be8cea00ede0602af8442a8fd9c7cfc
el <el@gmail.com>
2022-06-24 11:15:04 +0300

    last

---
+++ script.js
@@ -0,0 +1,108 @@
+let product = {
+    plainBurger: {
+        name: 'Гамбургер простой',
+        price: 10000,
+        kcall: 400,
+        amount: 0,
+        get SUMM(){
+            return this.price * this.amount
+        },
+        get KCALL(){
+            return this.kcall * this.amount
+        }
+    },
+    freshBurger: {
+        name: 'Гамбургер FRESH',
+        price: 20500,
+        kcall: 800,
+        amount: 0,
+        get SUMM(){
+            return this.price * this.amount
+        },
+        get KCALL(){
+            return this.kcall * this.amount
+        }
+    },
+    freshBurger: {
+        name: 'FRESH COMBO',
+        price: 31900,
+        kcall: 1200,
+        amount: 0,
+        get SUMM(){
+            return this.price * this.amount
+        },
+        get KCALL(){
+            return this.kcall * this.amount
+        }
+    },
+}
+
+let extraProduct = {
+    doubleMayonnaise:{
+        name: 'Двойной майонез',
+        price: 2000,
+        kcall: 100,
+    },
+    lettuce:{
+        name: 'Салатный лист',
+        price: 4000,
+        kcall: 20,
+    },
+    cheese:{
+        name: 'Сыр',
+        price: 5000,
+        kcall: 130,
+    },
+}
+
+let btnPlusOrMinus = document.querySelectorAll('.main__product-btn'),
+    checkExtraProduct = document.querySelectorAll('.main__product-checkbox'),
+    addCart = document.querySelector('.addCart'),
+    receipt = document.querySelector('.receipt'),
+    receiptWindow = document.querySelector('.receipt__window'),
+    receiptOut = document.querySelector('.receipt__window-out'),
+    payBtn = document.querySelector('.receipt__window-btn');
+    
+btnPlusOrMinus.forEach((btn) => {
+    btn.addEventListener('click', function() {
+        plusOrMinus(this)
+    })
+    let interval = 0
+    btn.addEventListener('mousedown', function() {
+        interval = setInterval(() => plusOrMinus(this), 100)
+    })
+    btn.addEventListener('mouseup', function() {
+        clearInterval(interval)
+    })
+})
+
+function plusOrMinus (element){
+    //closeset подключается к ближайщему заданному родителю
+    //getAttribute() - берет значение указанного атрибута
+    let parentId = element.closest('.main__product').getAttribute('id')
+    let output = element.closest('.main__product').querySelector('.main__product-num')
+    let price = element.closest('.main__product').querySelector('.main__product-price span')
+    let kcall = element.closest('.main__product').querySelector('.main__product-kcall span')
+    
+    if(element.getAttribute('data-symbol') == '+') {
+        product[parentId].amount++
+    } else if(element.getAttribute('data-symbol') == '-' && product[parentId].amount > 0){
+        product[parentId].amount--
+    }
+    
+    output.innerHTML = product[parentId].amount
+    price.innerHTML = product[parentId].SUMM
+    kcall.innerHTML = product[parentId].KCALL
+}
+
+let levelStart = document.querySelector('.header__timer-extra')
+
+function levelUpdateRecursion(n){
+    n++
+    levelStart.innerHTML = n
+    if(n < 100){
+        setTimeout(() => levelUpdateRecursion(n), 99)
+    }
+}
+
+levelUpdateRecursion(0)
